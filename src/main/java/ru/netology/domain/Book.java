5166e59f198b29c13a5a88ffe62813ae38b0d01f
Vera Chikhareva <vera.chepovaya@mail.ru>
2021-02-08 16:58:32 +0300

    add tests

--- src/main/java/ru/netology/domain/Book.java
+++ src/main/java/ru/netology/domain/Book.java
@@ -1,21 +1,10 @@
 package ru.netology.domain;
 
 public class Book extends Product {
-    private String name;
     private String author;
 
     public Book(int id, String name, int price) {
         super(id, name, price);
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getAuthor() {
@@ -26,9 +15,9 @@ public class Book extends Product {
         this.author = author;
     }
 
-    public Book(int id, String name, int price, String name1, String author) {
+    public Book(int id, String name, int price, String author) {
         super(id, name, price);
-        this.name = name1;
         this.author = author;
     }
 }
+
