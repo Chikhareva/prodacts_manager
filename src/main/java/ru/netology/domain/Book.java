e6d7afe7e28b2a551be741088a0411ea84394384
Vera Chikhareva <vera.chepovaya@mail.ru>
2021-02-09 18:04:29 +0300

    initial commit

--- src/main/java/ru/netology/domain/Book.java
+++ src/main/java/ru/netology/domain/Book.java
@@ -14,8 +14,7 @@ public class Book extends Product {
     public void setAuthor(String author) {
         this.author = author;
     }
-
-
+    
     public Book(int id, String name, int price, String author) {
         super(id, name, price);
         this.author = author;
